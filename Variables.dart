fc32393fb2b4acfbd0fb70375ca0b668f1796ed9
Neha Mundase <106380291+neha-mundase20@users.noreply.github.com>
2024-05-10 23:06:09 +0530

    Variables.dart

---
+++ Variables.dart
@@ -0,0 +1,21 @@
+import 'dart:ffi';
+
+void main() {
+  int age = 30;
+  print(age);
+
+  double number = 1.24;
+  print(number);
+
+  String value = "Dart Essentials";
+  print(value);
+
+  bool isNight = false;
+  print(isNight);
+
+  /*'dynamic' is a variable where datatype of the variable can be changed*/
+  dynamic source = "The Alchemist";
+  print(source);
+  source = 32;
+  print(source);
+}
